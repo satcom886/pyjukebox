9e417c021749e552384cf08d57b14fe64d9100c3
satcom886 <rostik.medved@gmail.com>
2020-07-12 10:24:31 +0200

    Added the ability to choose the audio format

--- pyjukebox.py
+++ pyjukebox.py
@@ -13,7 +13,7 @@ last_song_file_location = tmp_directory + "/lastsong.txt"
 
 def download_loop():
     while getattr(download_thread, "do_run", True):
-        subprocess.run(["youtube-dl", "--no-overwrites", "--ignore-errors", "--extract-audio", "--audio-quality=0", "--audio-format=opus", "--yes-playlist", "--output", tmp_directory + "/%(playlist_index)s.%(ext)s", "--download-archive", tmp_directory + "/downloaded.txt", config["playlist_url"]])
+        subprocess.run(["youtube-dl", "--no-overwrites", "--ignore-errors", "--extract-audio", "--audio-quality=0", "--audio-format=" + config["audio_format"], "--yes-playlist", "--output", tmp_directory + "/%(playlist_index)s.%(ext)s", "--download-archive", tmp_directory + "/downloaded.txt", config["playlist_url"]])
         time.sleep(config["playlist_update_rate"])
 
 def player_loop():
@@ -28,7 +28,7 @@ def player_loop():
     elif config["restore_last_session"] is False:
         counter = 0
     while True:
-        songs = sorted(glob.glob(tmp_directory + "/*.opus"))
+        songs = sorted(glob.glob(tmp_directory + "/*." + config["audio_format"]))
         # This is just for checking if MPV should run with the GUI.
         if config["gui_enabled"] is True:
             try:
